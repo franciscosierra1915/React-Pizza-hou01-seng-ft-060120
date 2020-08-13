ae4f4b79278fab70448a59fc77f09df3d26a575a
franciscosierra1915 <fransierra.s.15@gmail.com>
2020-08-13 15:52:13 -0500

    no onchange

--- src/App.js
+++ src/App.js
@@ -2,13 +2,68 @@ import React, { Component, Fragment } from 'react';
 import Header from './components/Header'
 import PizzaForm from './components/PizzaForm'
 import PizzaList from './containers/PizzaList'
+
+const pizzasurl = 'http://localhost:3000/pizzas'
+
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      pizzas: [],
+      editablePizza: ''
+    }
+  }
+
+  componentDidMount() {
+    this.fetchPizzas()
+  }
+
+  fetchPizzas() {
+    fetch(pizzasurl)
+    .then(r => r.json())
+    .then(pizzas => this.setState({pizzas}))
+  }
+  editPizza = (myPizza) => {
+    this.setState({editablePizza: myPizza})
+  }
+  handleSubmit = (e) => {
+    e.preventDefault
+    let form = e.currentTagret
+    let veggieOrNot 
+    if (e.target.veggie === true) {
+      veggieOrNot = true
+    } else {
+      veggieOrNot = false
+    }
+    let editablePizza = this.state.editablePizza
+    let pizzaWithEdits = {
+      topping: e.target.topping.value,
+      size: e.target.size.value,
+      vegetarian: veggieOrNot
+    }
+    let objecConfig = {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify(pizzaWithEdits)
+    }
+    fetch(`${pizzasurl}/${editablePizza}`, objecConfig).then(r => this.fetchPizzas())
+    form.reset()
+  }
+
   render() {
     return (
       <Fragment>
         <Header/>
-        <PizzaForm/>
-        <PizzaList/>
+        <PizzaForm 
+        myPizza={this.state.editablePizza}
+        handleSubmit={this.handleSubmit}/>
+        <PizzaList 
+        pizzas={this.state.pizzas}
+        editPizza={this.editPizza}/>
       </Fragment>
     );
   }
--- src/components/PizzaForm.js
+++ src/components/PizzaForm.js
@@ -1,41 +1,43 @@
 import React from "react"
 
-const PizzaForm = () => {
+const PizzaForm = (props) => {
+  const pizza = props.myPizza
+
   return(
+    <form onSubmit={(e) => props.handleSubmit(e)}>
       <div className="form-row">
         <div className="col-5">
-            <input type="text" className="form-control" placeholder="Pizza Topping" value={
-                //Pizza Topping Should Go Here
-                null
-              }/>
+            <input type="text" className="form-control" placeholder="Pizza Topping" name='topping' value={pizza.topping}/>
         </div>
         <div className="col">
-          <select value={null} className="form-control">
+          <select value={pizza.size} className="form-control" name='size'>
             <option value="Small">Small</option>
             <option value="Medium">Medium</option>
             <option value="Large">Large</option>
           </select>
         </div>
         <div className="col">
-          <div className="form-check">
-            <input className="form-check-input" type="radio" value="Vegetarian" checked={null}/>
+          <div className="form-check" >
+            <input className="form-check-input" type="radio" value="Vegetarian" name='veggie' checked={pizza.vegetarian ? true : ''}/>
             <label className="form-check-label">
               Vegetarian
             </label>
           </div>
           <div className="form-check">
-            <input className="form-check-input" type="radio" value="Not Vegetarian" checked={null}/>
+            <input className="form-check-input" type="radio" value="Not Vegetarian" name='not veggie'checked={pizza.vegetarian ? '' : true}/>
             <label className="form-check-label">
               Not Vegetarian
             </label>
           </div>
         </div>
         <div className="col">
-          <button type="submit" className="btn btn-success" onClick={console.log}>Submit</button>
+          <button type="submit" className="btn btn-success" name="submit">Submit</button>
         </div>
       </div>
+      </form>
 
   )
 }
 
 export default PizzaForm
+
